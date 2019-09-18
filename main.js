5768e6e82aa83640bcb8e96b9bcd7c19282b6b01
Miguel Fernández Carratalá <miguelfdezc99@gmail.com>
2019-09-18 18:31:50 +0200

    Added initialize, clear and load functions

--- main.js
+++ main.js
@@ -3,3 +3,49 @@ document.addEventListener('keydown', function(event) {
         console.log("jump");
     }
 })
+
+var imgRex, imgCloud, imgCactus, imgGround;
+
+function loadImages() {
+    imgCactus = new Image();
+    imgCloud = new Image();
+    imgGround = new Image();
+    imgRex = new Image();
+
+    imgCactus.src = 'img/cactus.png';
+    imgCloud.src = 'img/cloud.png';
+    imgGround.src = 'img/ground.png';
+    imgRex.src = 'img/rex.png';
+}
+
+var width = 700;
+var height = 300;
+
+var canvas, context;
+
+function initialize() {
+    canvas = document.getElementById('canvas');
+    context = canvas.getContext('2d');
+    loadImages();
+};
+
+
+
+function clearCanvas() {
+    canvas.width = width;
+    canvas.height = height;
+}
+
+// ------------------------------------------------------
+// Main loop
+var FPS = 10;
+setInterval(function() {
+    main();
+},1000/10);
+
+
+
+
+function main() {
+    clearCanvas();
+}
